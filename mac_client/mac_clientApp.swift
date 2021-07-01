43e0a9dd039f93b9294f94e2627cd989ea27580a
Jack Ryan <jackryan1998@hotmail.com>
2021-07-01 20:54:39 +1000

    Initial Commit

---
+++ mac_client/mac_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_clientApp.swift
+//  mac_client
+//
+//  Created by Jack Ryan on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct mac_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
